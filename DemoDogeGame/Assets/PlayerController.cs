7960c5c56e2546c8b2c95c9810657b7715dd92e2
jasonoh1998 <ocmosk@gmail.com>
2021-04-18 07:57:03 -0400

    made player move around

---
+++ DemoDogeGame/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    public float speed = 8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //find Rigidbody component and give it to playerRigidbody
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // reads the player input of x and z axis
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        // calculate the exact speed by multiplying to the direction inputed
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        // Vector3 speed is created as (xSpeed, 0, zSpeed)
+        Vector3 newVelocity = new Vector3(xSpeed, 0, zSpeed);
+
+        // input newVelocity to Rigidbody
+        playerRigidbody.velocity = newVelocity;
+    }
+
+    public void Dead()
+    {
+        // Player Object is deactivated
+        gameObject.SetActive(false);
+    }
+}

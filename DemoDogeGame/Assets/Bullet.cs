c2ed1f10bfb4691fe4702072e269a1ee2a5f18f3
jasonoh1998 <ocmosk@gmail.com>
2021-04-19 11:24:19 -0400

    Bullet Spawner and Bullet is made
    Bullet Spawner shoots bullet by detecting the players location.

---
+++ DemoDogeGame/Assets/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 4f; // speed of bullet
+    private Rigidbody bulletRigidbody; // Rigidbody component for bullets
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // give Rigidbody component to bulletRigidbody
+        bulletRigidbody = GetComponent<Rigidbody>();
+
+        // Rigidbody speed = forward * speed
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        //destroy gameObject in 3 seconds
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // if the tag of object is Player
+        if(other.tag == "Player")
+        {
+            // bring PlayerController component
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            // if success bring playerController
+            if(playerController != null)
+            {
+                // run Death() method from player
+                playerController.Dead();
+            }
+        }
+    }
+}
---
+++ DemoDogeGame/Assets/BulletSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab; // bullet prefab
+    public float spawnRateMin = 0.5f; // minimum spawn rate
+    public float spawnRateMax = 3f; // maximum spawn rate
+
+    private Transform target; // target to shoot bullets
+    private float spawnRate; // overal spawn rate
+    private float timeAfterSpawn; // time after recent spawn of bullet
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // reset the stored spawn time to 0
+        timeAfterSpawn = 0f;
+        // create random spawn rate between min and max spawn rate
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        // find object with PlayerController
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update timeAfterSpawn
+        timeAfterSpawn += Time.deltaTime;
+
+        // if timeAfterSpawn was larger than the randomized spawn rate
+        if (timeAfterSpawn >= spawnRate)
+        {
+            // reset timeAfterSpawn
+            timeAfterSpawn = 0f;
+
+            // make bulletPrefab copy
+            // use transform.position and transform.rotation to create it
+            GameObject bullet
+                = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            // make the bullets face the target
+            bullet.transform.LookAt(target);
+            // create new randomized spawn rate using spawnRateMin and spawnRateMax variables
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
+    }
+}
